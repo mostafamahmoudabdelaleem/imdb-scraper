4f7c676bdacdc8fad61e04d8fab925e783f948d5
Mostafa <mostafamahmoudabdelalim@hotmail.com>
2019-11-25 00:37:40 +0200

    Created Movie Class

---
+++ src/Movie.js
@@ -0,0 +1,16 @@
+class Movie{
+
+    id = null;                  //  ex. "tt7949218"
+    name = null;                //  ex. SEE
+    release_date = null;        //  ex. 1 November 2019
+    rating = null;              //  ex. 7.7
+    genres = null;              //  ex. ["Action", "Drama", "Sci-Fi", ...]
+    storyline = null;           //  ex. "Far in a dystopian future, the human race has lost the sense of sight..."
+    poster_photo_url = null;    //  ex. "https://m.media-amazon.com/images/*.jpg"
+    cover_photo_url = null;     //  ex. "https://m.media-amazon.com/images/*.jpg"
+    country = null;             //  ex. "USA"
+    language = null;            //  ex. "English"
+    runtime = null;             //  ex. "60 Min"
+    cast = null;                //  ex. [Actor, Actor, ...]
+
+}
