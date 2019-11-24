24b5dcacc05dafb6b8aa9f1374bf38f01e51faee
Mostafa <mostafamahmoudabdelalim@hotmail.com>
2019-11-25 00:37:56 +0200

    Created Actor Class

---
+++ src/Actor.js
@@ -0,0 +1,7 @@
+class Actor{
+
+    name = null;        //  ex. "Jason Momoa"
+    role_name = null;   //  ex. "Baba Vose"
+    photo_url = null;   //  ex. "https://m.media-amazon.com/images/*.jpg"
+
+}
